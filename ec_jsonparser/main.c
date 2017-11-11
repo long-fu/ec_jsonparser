94205468c2f8d7baffbbf901aa23a370bb73d805
EJunYan <longjunyan913@gmail.com>
2017-11-11 15:41:03 +0800

    解析测试代码

--- ec_jsonparser/main.c
+++ ec_jsonparser/main.c
@@ -11,46 +11,156 @@
 #include "jsonparse.h"
 
 
+char jsontree[] = "{\
+    \"act\": \"server\",\
+    \"linkid\": \"xxxxxxxxx\",\
+    \"type\": \"0|1\",\
+    \"switchList\": [\
+                   {\
+                   \"times\": \"xxxxxx\",\
+                   \"time\": \"2017-10-14 15:42:00\",\
+                   \"enable\": \"0|1\"\
+                   },\
+                   {\
+                   \"times\": \"--------\",\
+                   \"time\": \"2017-10-14 15:42:00\",\
+                   \"enable\": \"0|1\"\
+                   }\
+                   ],\
+    \"sceneList\": [\
+                  {\
+                  \"time\": \"2017-10-14 15:42:00\",\
+                  \"enable\": \"0|1\"\
+                  },\
+                  {\
+                  \"time\": \"2017-10-14 15:42:00\",\
+                  \"enable\": \"0|1\"\
+                  }\
+                  ]\
+}";
+
+//  这个SBjson解析裤看不懂
+
 char json[] = "{\"name\":\"long\",\"user\":12}";
-int main(int argc, const char * argv[]) {
-    struct jsonparse_state state1;
-    char buf[16] = { 0 };
-    printf("%s\n",json);
-    jsonparse_setup((struct jsonparse_state *)&state1, json, (int)strlen(json));
-    int type;
-    while ((type = jsonparse_next(&state1)) != 0) {
-        if (type == JSON_TYPE_PAIR_NAME)
-        {
-            if (jsonparse_strcmp_value(&state1, "name") == 0)
-            {
-                char buf[16] = { 0 };
-                jsonparse_next(&state1);
-                jsonparse_next(&state1);
-                jsonparse_copy_value(&state1, buf, sizeof(buf));
-                printf("buff %s\n",buf);
-            }
-            
-            if (jsonparse_strcmp_value(&state1, "user") == 0)
-            {
-                char buf[4]= { 0 };
-                int s;
-                jsonparse_next(&state1);
-                jsonparse_next(&state1);
+int lmainl(int argc, const char * argv[]) {
+//    struct jsonparse_state state1;
+//    printf("%s\n",json);
+//    jsonparse_setup((struct jsonparse_state *)&state1, json, (int)strlen(json));
+//    int type;
+//    while ((type = jsonparse_next(&state1)) != 0) {
+//        if (type == JSON_TYPE_PAIR_NAME)
+//        {
+//            if (jsonparse_strcmp_value(&state1, "name") == 0)
+//            {
+//                char buf[16] = { 0 };
+//                jsonparse_next(&state1);
 //                jsonparse_next(&state1);
 //                jsonparse_copy_value(&state1, buf, sizeof(buf));
-                s = jsonparse_get_value_as_int(&state1);
-                printf(" ---- == = %d\n",s);
+//                printf("buff %s\n",buf);
+//            }
+//
+//            if (jsonparse_strcmp_value(&state1, "user") == 0)
+//            {
+//                int s;
+//                jsonparse_next(&state1);
+//                jsonparse_next(&state1);
+//                s = jsonparse_get_value_as_int(&state1);
+//                printf(" ---- == = %d\n",s);
+//            }
+//        }
+//    }
+    
+    
+    struct jsonparse_state json_state;
+
+    printf("%s\n",jsontree);
+
+    jsonparse_setup(&json_state, jsontree, strlen(jsontree));
+    int type;
+    while((type = jsonparse_next(&json_state)) != 0) {
+        if (type == JSON_TYPE_PAIR_NAME) {
+        if (jsonparse_strcmp_value(&json_state, "act") == 0)
+        {
+            char buf[16] = { 0 };
+            jsonparse_next(&json_state);
+            jsonparse_next(&json_state);
+            jsonparse_copy_value(&json_state, buf, sizeof(buf));
+            printf("act buff: %s --\n",buf);
+        }
+
+        if (jsonparse_strcmp_value(&json_state, "linkid") == 0)
+        {
+            char buf[16] = { 0 };
+            jsonparse_next(&json_state);
+            jsonparse_next(&json_state);
+            jsonparse_copy_value(&json_state, buf, sizeof(buf));
+            printf("linkid buff: %s\n",buf);
+        }
+
+        if (jsonparse_strcmp_value(&json_state, "type") == 0)
+        {
+            char buf[16] = { 0 };
+            jsonparse_next(&json_state);
+            jsonparse_next(&json_state);
+            jsonparse_copy_value(&json_state, buf, sizeof(buf));
+            printf("type buff: %s\n",buf);
+        }
+        
+        if (type == JSON_TYPE_ARRAY && jsonparse_strcmp_value(&json_state, "switchList"))
+        {
+            printf("switchList JSON_TYPE_ARRAY\n");
+            if (jsonparse_strcmp_value(&json_state, "times") == 0)
+            {
+                char buf[16] = { 0 };
+                jsonparse_next(&json_state);
+                jsonparse_next(&json_state);
+                jsonparse_copy_value(&json_state, buf, sizeof(buf));
+                printf("times buff %s\n",buf);
+            }
+
+            if (jsonparse_strcmp_value(&json_state, "time") == 0)
+            {
+                char buf[16] = { 0 };
+                jsonparse_next(&json_state);
+                jsonparse_next(&json_state);
+                jsonparse_copy_value(&json_state, buf, sizeof(buf));
+                printf("times buff %s\n",buf);
+            }
+
+            if (jsonparse_strcmp_value(&json_state, "enable") == 0)
+            {
+                char buf[16] = { 0 };
+                jsonparse_next(&json_state);
+                jsonparse_next(&json_state);
+                jsonparse_copy_value(&json_state, buf, sizeof(buf));
+                printf("enable buff %s\n",buf);
             }
         }
+        
+        if (type == JSON_TYPE_ARRAY && jsonparse_strcmp_value(&json_state, "sceneList"))
+        {
+            printf("sceneList JSON_TYPE_ARRAY\n");
+            if (jsonparse_strcmp_value(&json_state, "time") == 0)
+            {
+                char buf[16] = { 0 };
+                jsonparse_next(&json_state);
+                jsonparse_next(&json_state);
+                jsonparse_copy_value(&json_state, buf, sizeof(buf));
+                printf("times buff %s\n",buf);
+            }
+            
+            if (jsonparse_strcmp_value(&json_state, "enable") == 0)
+            {
+                char buf[16] = { 0 };
+                jsonparse_next(&json_state);
+                jsonparse_next(&json_state);
+                jsonparse_copy_value(&json_state, buf, sizeof(buf));
+                printf("enable buff %s\n",buf);
+            }
+
+        }
+        }
     }
-    
-    
-//    int r = jsonparse_strcmp_value(&state1, "user");
-//    printf(" -- r -- %d\n",r);
-//    jsonparse_next(&state1);
-//    jsonparse_next(&state1);
-//    jsonparse_copy_value(&state1, buf, sizeof(buf));
-//    printf("%s\n",buf);
     return 0;
 }
 
